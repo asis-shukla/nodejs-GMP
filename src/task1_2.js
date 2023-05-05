5adae30bf3855b8bbdd681a570877142c69ac324
Ashish <ashishfun008@gmail.com>
2023-05-06 02:24:44 +0530

    updated task 2

--- src/task1_2.js
+++ src/task1_2.js
@@ -1,8 +1,18 @@
 const fs = require("fs");
 const csv = require("csvtojson");
 
-const readStream = fs.createReadStream("src/books.csv", "utf8");
+const readStream = fs.createReadStream("src/books.csv", 'utf8');
 const writeStream = fs.createWriteStream("src/books.txt", "utf8");
 
-readStream.pipe(csv()).pipe(writeStream);
-
+readStream
+    .on("error", (err) => {
+        console.log("error", err);
+    })
+    .pipe(csv()).setEncoding("utf8")
+    .on("data", (line) => {
+        console.log("line", line);
+    })
+    .pipe(writeStream)
+    .on("close", () => {
+        console.log("end of file");
+    });
