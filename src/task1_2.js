dace76a3b8b3c136c7d85f06c559a15f16c02385
Ashish <ashishfun008@gmail.com>
2023-05-06 08:35:26 +0530

    added support for es6 imports

--- src/task1_2.js
+++ src/task1_2.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const csv = require("csvtojson");
+import fs from "fs";
+import csv from "csvtojson";
 
 const readStream = fs.createReadStream("src/books.csv", 'utf8');
 const writeStream = fs.createWriteStream("src/books.txt", "utf8");
